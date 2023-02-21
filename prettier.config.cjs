09aa057b953e207ca2edb30d2792cc1be17793f0
Dima <dimasakharov@icloud.com>
2023-02-21 17:24:40 +0300

    load

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  printWidth: 80,
-  tabWidth: 4,
-  semi: true,
+    printWidth: 80,
+    tabWidth: 4,
+    semi: true,
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,20 @@
 import { Servise } from "./Servise";
+import "../scss/style.scss";
 
-const weather = document.querySelector("#root .weather");
+const weather = document.querySelector(".weather");
 const input = document.querySelector("#input");
+
+Servise.get("Moscow").then((data) => console.log(data));
 
 const markup = (data) => {
     return `
-        <div>
-            <h1>${data.location.name}</h1>
-            <p>
-            ${data.current.cloud}
-            </p>
+        <div class="weather__info">
+            <h1 class="weather__h1">${data.location.name}</h1>
+            <h2 class="weather__c">${data.current.temp_c}<h2>
+                <div class="weather__options">
+                    <p class="weather__speed">${data.current.wind_kph}</p>
+                    <p class="weather__humidity">${data.current.humidity}</p>
+                </div>
         </div>
     `;
 };
